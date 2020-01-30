961b367c81b9f0856501b8037127bf8744af8b06
Jampa Uchoa <joaopedrouchoa@hotmail.com>
2020-01-30 01:46:20 -0300

    experiment with evaluator

--- scorer/views.py
+++ scorer/views.py
@@ -1,8 +1,12 @@
 from django.shortcuts import render
-
 # Create your views here.
-from django.http import HttpResponse
-
+from django.http import HttpResponse, JsonResponse
+from ML.algorithms import Classifier
 
 def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+
+    redacao = Classifier(["Hello, world. You're at the polls index."])
+    print(redacao)
+    print(redacao.linear_score())
+
+    return JsonResponse({"success": True})
