86b2afd5af0126bfa75c8981199eafc2b968f64a
Tiago de Lima <tiago.blima02@gmail.com>
2020-01-31 07:42:38 -0300

    A view voltando ao que era antes

--- scorer/views.py
+++ scorer/views.py
@@ -1,12 +1,12 @@
 from django.shortcuts import render
 from django.http import HttpResponse, JsonResponse
 from django.views.decorators.csrf import csrf_exempt
-from ML.algorithms import CoherenceClf
+
 #from ML.algorithms import Classifier
 from traditional.Classifiers import Classifiers as Traditional
 import json
 
-@csrf_exempt 
+@csrf_exempt
 def index(request):
 
     if request.method == 'POST':
@@ -18,11 +18,10 @@ def index(request):
     redacao = Traditional(essay)
     score_svm = redacao.SVM_Modeler()
     score_dt = redacao.Tree_Modeler()
-    score_coh = CoherenceClf(essay).mean_score()
+
     scores = [
         {"algorithm": "SVM", "score": score_svm},
         {"algorithm": "Decision Tree", "score": score_dt},
-        {"algorithm": "mean_score", "score": score_coh}
     ]
 
     return JsonResponse({"success": True, "scores": scores})
